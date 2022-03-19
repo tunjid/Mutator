1634e9bf5bd5ab674e1ddb680b2534ae0576f334
Adetunji Dahunsi <tjdah100@gmail.com>
2022-03-19 08:54:46 -0400

    Bumb gradle and AGP versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,9 +33,6 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        val libs = project.extensions.getByType<VersionCatalogsExtension>()
-            .named("libs") as org.gradle.accessors.dm.LibrariesForLibs
-
         classpath(libs.android.gradlePlugin)
         classpath(libs.kotlin.gradlePlugin)
         classpath(libs.dokka.gradlePlugin)
